f0352e9e6a1f2dc3453b1b31988deae6ec9905c7
CrisL1990 <lucchetta.cris@gmail.com>
2022-02-11 17:09:02 +0100

    prove

--- main.js
+++ main.js
@@ -2,24 +2,35 @@ const root = new Vue(
     {
         el: '#root',
 
+        
+
         data:{
             input: "",
+
+            done: false,
+
             items: ["Alzarsi", "Fare colazione", "Studiare"]
         },
 
         methods: {
             addTask: function(){
-
                 if(this.input != ""){
                     this.items.push(this.input);
                     this.input = "";  
-                }
-                      
+                }                
             },
 
             removeTask: function(index){
                 this.items.splice(index, 1);
+            },
+
+            textLine: function(index){
+
+                this.done = true;
+                
             }
+
+           
         }
     }
 )
